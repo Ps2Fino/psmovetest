c9b7cd7e8581bc765606364ae7672d07656256d8
Dan Finnegan <dan.finnegan@somethinelse.com>
2014-06-12 09:18:03 +0300

    Refactored. Will test today

--- test.c
+++ test.c
@@ -8,160 +8,95 @@
 typedef struct game_struct {
 	PSMove *controller;
 	char *serial_number;
-
+	int counter;
 } Game_Struct;
 
 void init(int id, Game_Struct *addr_struct)
 {
 	addr_struct = (Game_Struct*) malloc(sizeof(Game_Struct));
-	addr_struct->controller = (PSMove*) malloc(sizeof(PSMove));
+	addr_struct->controller = NULL;
 	addr_struct->controller = psmove_connect_by_id(id);
 
-	addr_struct->serial_number = psmove_get_serial(addr_struct->controller_1);
+	addr_struct->serial_number = NULL;
+	enum PSMove_Connection_Type type = psmove_connection_type(addr_struct->controller);
+	if (type == Conn_Bluetooth)
+		addr_struct->serial_number = psmove_get_serial(addr_struct->controller);
+	else
+		addr_struct->serial_number = NULL;
+
+	addr_struct->counter = 0;
+}
+
+void destroy(Game_Struct *addr_struct)
+{
+	psmove_disconnect(addr_struct->controller);
+	if (addr_struct->serial_number != NULL)
+	{
+		free(addr_struct->serial_number);
+	}
 }
 
 int main (int argc, char **argv)
 {
 	int numControllers = atoi(argv[1]);
 
-	Game_Struct *game_structs = NULL;
+	Game_Struct **game_structs = NULL;
 	game_structs = (Game_Struct**) malloc(numControllers * sizeof(Game_Struct*));
 
 	for (int i=0; i < numControllers; ++i)
 	{
-		game_structs[i] = (Game_Struct*) malloc(sizeof(Game_Struct));
-		game_structs[i]->controller = 
-		controllerArray[i] = psmove_connect_by_id(i);
-		// Write the bluetooth address of the controller to the terminal
-		enum PSMove_Connection_Type type = psmove_connection_type(controller_1);
-		type == Conn_Bluetooth ? printf("Connection type: bluetooth\n") : printf("Connection type: USB\n");
-
-		char *serial_number = NULL;
-		if (type == Conn_Bluetooth)
-		{
-			serial_number = psmove_get_serial(controller_1);
-			printf("The serial number is: %s\n", serial_number);
-		}
+		game_structs[i] = NULL;
+		init(i, game_structs[i]);
 	}
 
-	// psmove_enable_orientation(controller_1, PSMove_True);
 	float x, y, z;
-
-	float counter_1 = 0;
-	float counter_2 = 0;
-	float counter_3 = 0;
-
 	while(1) {
-		if (psmove_poll(controller_1))
+		for (int i=0; i < numControllers; ++i)
 		{
-			printf("We got data\n");
-			// Grab some data and normalise it
-			// psmove_get_accelerometer(controller_1, &x, &y, &z);
-			psmove_get_accelerometer_frame(controller_1, Frame_SecondHalf, &x, &y, &z);
-			double x_sqr = pow(x, 2);
-			double y_sqr = pow(y, 2);
-			double z_sqr = pow(z, 2);
-
-			double norm = sqrt(x_sqr + y_sqr + z_sqr);
-			// printf("Norm: %.2f\tX: %d\tY: %d\tZ:%d\n", norm, x, y, z);
-			// printf("Norm: %.2f\tX: %.2f\tY: %.2f\tZ:%.2f\n", norm, x, y, z);
-
-			if (norm >= 2)
+			PSMove *controller = game_structs[i]->controller;
+			if (psmove_poll(controller))
 			{
-				counter_1 += 0.1;
-				char red = (char) floor(counter_1);
-				printf("RED: %.d\n", (int) red);
+				psmove_get_accelerometer_frame(controller, Frame_SecondHalf, &x, &y, &z);
+				double x_sqr = pow(x, 2);
+				double y_sqr = pow(y, 2);
+				double z_sqr = pow(z, 2);
+				int counter = game_structs[i]->counter;
+
+				double norm = sqrt(x_sqr + y_sqr + z_sqr);
+				if (norm >= 2)
+				{
+					counter += 0.1;
+					game_structs[i]->counter = counter;
+					char red = (char) floor(counter);
+					printf("Controller %d: %.d\n", i, (int) red);
+				}
+
+				if (game_structs[i]->counter > 100)
+				{
+					break;
+				}
+
+				psmove_set_leds(controller, (char) floor(counter), 0, 0);
+				psmove_update_leds(controller);	
 			}
-
-			if (counter_1 > 100)
-			{
-				break;
-			}
-
-			psmove_set_leds(controller_1, (char) floor(counter_1), 0, 0);
-			psmove_update_leds(controller_1);			
-		}
-
-		if (psmove_poll(controller_2))
-		{
-			printf("We got data\n");
-			// Grab some data and normalise it
-			// psmove_get_accelerometer(controller_1, &x, &y, &z);
-			psmove_get_accelerometer_frame(controller_2, Frame_SecondHalf, &x, &y, &z);
-			double x_sqr = pow(x, 2);
-			double y_sqr = pow(y, 2);
-			double z_sqr = pow(z, 2);
-
-			double norm = sqrt(x_sqr + y_sqr + z_sqr);
-			// printf("Norm: %.2f\tX: %d\tY: %d\tZ:%d\n", norm, x, y, z);
-			// printf("Norm: %.2f\tX: %.2f\tY: %.2f\tZ:%.2f\n", norm, x, y, z);
-
-			if (norm >= 2)
-			{
-				counter_2 += 0.1;
-				char red = (char) floor(counter_2);
-				printf("RED: %.d\n", (int) red);
-			}
-
-			if (counter_2 > 100)
-			{
-				break;
-			}
-
-			psmove_set_leds(controller_2, (char) floor(counter_2), 0, 0);
-			psmove_update_leds(controller_2);			
-		}
-
-		if (psmove_poll(controller_3))
-		{
-			printf("We got data\n");
-			// Grab some data and normalise it
-			// psmove_get_accelerometer(controller_1, &x, &y, &z);
-			psmove_get_accelerometer_frame(controller_3, Frame_SecondHalf, &x, &y, &z);
-			double x_sqr = pow(x, 2);
-			double y_sqr = pow(y, 2);
-			double z_sqr = pow(z, 2);
-
-			double norm = sqrt(x_sqr + y_sqr + z_sqr);
-			// printf("Norm: %.2f\tX: %d\tY: %d\tZ:%d\n", norm, x, y, z);
-			// printf("Norm: %.2f\tX: %.2f\tY: %.2f\tZ:%.2f\n", norm, x, y, z);
-
-			if (norm >= 2)
-			{
-				counter_3 += 0.1;
-				char red = (char) floor(counter_3);
-				printf("RED: %.d\n", (int) red);
-			}
-
-			if (counter_3 > 100)
-			{
-				break;
-			}
-
-			psmove_set_leds(controller_3, (char) floor(counter_3), 0, 0);
-			psmove_update_leds(controller_3);			
 		}
 
 	}
 
 	// decide who to blink
 	PSMove *blink_controller = NULL;
-	if (counter_1 > 100)
+	for (int i = 0; i < numControllers; ++i)
 	{
-		blink_controller = controller_1;
-	}
-	else if(counter_2 > 100) 
-	{
-		blink_controller = controller_2;
-	}
-	else
-	{
-		blink_controller = controller_3;
+		if (game_structs[i]->counter > 100)
+		{
+			blink_controller = game_structs[i]->controller;
+			break;
+		}
 	}
 
-	while (1)
+	// Flash green
+	for (int i = 0; i < 5; ++i)
 	{
-		// Flash green
 		psmove_set_leds(blink_controller, 0, 255, 0);
 		psmove_update_leds(blink_controller);
 		sleep(1);
@@ -170,19 +105,12 @@ int main (int argc, char **argv)
 		sleep(1);
 	}
 
-	// Disconnect the controller
-	psmove_disconnect(controller_1);
-	psmove_disconnect(controller_2);
-	psmove_disconnect(controller_3);
+	// Disconnect and free
+	for (int i = 0; i < numControllers; ++i)
+	{
+		destroy(game_structs[i]);
+	}
 
-	free(controller_1);
-	free(controller_2);
-	free(controller_3);
-	if (serial_number != NULL) free(serial_number);
-	controller_1 = NULL;
-	controller_2 = NULL;
-	controller_3 = NULL;
 	blink_controller = NULL;
-	serial_number = NULL;
 	return 0;
 }
