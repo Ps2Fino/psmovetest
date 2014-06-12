d5f104202148a15681246a94d46993b8facc2584
Dan Finnegan <dan.finnegan@somethinelse.com>
2014-06-12 12:28:07 +0300

    New mechanic with don't move woring

--- test.c
+++ test.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
+#include <time.h>
 
 #include "psmoveapi/psmove.h"
+
+enum State {
+	MOVE,
+	DONT_MOVE,
+	FINITO
+};
 
 typedef struct game_struct {
 	PSMove *controller;
 	char *serial_number;
 	float counter;
+	float dont_move_counter;
+	int state;
+	int dont_move_var;
 } Game_Struct;
 
 void init(int id, Game_Struct *addr_struct)
@@ -28,6 +38,9 @@ void init(int id, Game_Struct *addr_struct)
 		addr_struct->serial_number = NULL;
 	}
 
+	addr_struct->state = MOVE;
+	addr_struct->dont_move_counter = 5;
+	addr_struct->dont_move_var = rand() % 100 + 1;
 	addr_struct->counter = 0;
 }
 
@@ -40,8 +53,16 @@ void destroy(Game_Struct *addr_struct)
 	}
 }
 
+void print_debug(Game_Struct game_struct, int id)
+{
+	printf("%d\tDon't move variable: %d\n", id, game_struct.dont_move_var);
+	printf("%d\tCounter value: %.2f\n", id, game_struct.counter);
+}
+
 int main (int argc, char **argv)
 {
+	// Set the seed for the rand gen
+	srand(time(NULL));
 	int numControllers = atoi(argv[1]);
 	printf("Number of controllers: %d\n", numControllers);
 	// return 0;
@@ -58,51 +79,78 @@ int main (int argc, char **argv)
 
 	printf("game_structs[0] serial_number: %s\n", game_structs[0].serial_number);
 
+	int doLoop = 1;
 	float x, y, z;
-	while(1) {
+	while(doLoop) {
 		for (int i=0; i < numControllers; ++i)
 		{
 			PSMove *controller = game_structs[i].controller;
 			if (psmove_poll(controller))
 			{
 				psmove_get_accelerometer_frame(controller, Frame_SecondHalf, &x, &y, &z);
-				double x_sqr = pow(x, 2);
-				double y_sqr = pow(y, 2);
-				double z_sqr = pow(z, 2);
 
-				double norm = sqrt(x_sqr + y_sqr + z_sqr);
-				printf("Norm: %.2f\n", norm);
-				if (norm >= 2)
+				// Check for the change in state
+				if (game_structs[i].state == MOVE
+					&& game_structs[i].counter > game_structs[i].dont_move_var)
 				{
-					game_structs[i].counter += 0.1;
-					char red = (char) floor(game_structs[i].counter);
-					printf("Controller %d rising: %d\n", i, (int) red);
+					game_structs[i].state = DONT_MOVE;
 				}
 
-				if (game_structs[i].counter > 100)
+				if (game_structs[i].state == MOVE 
+					|| game_structs[i].state == FINITO)
 				{
-					break;
+					double x_sqr = pow(x, 2);
+					double y_sqr = pow(y, 2);
+					double z_sqr = pow(z, 2);
+
+					double norm = sqrt(x_sqr + y_sqr + z_sqr);
+					printf("Norm: %.2f\n", norm);
+
+					if (norm >= 2)
+					{
+						game_structs[i].counter += 0.1;
+						// char red = (char) floor(game_structs[i].counter);
+						// printf("Controller %d rising: %d\n", i, (int) red);
+					}
+					if (game_structs[i].counter > 20)
+					{
+						doLoop = 0;
+						break;
+					}
+
+					if (game_structs[i].counter > 0.02)
+					{
+						game_structs[i].counter -= 0.02;
+					}
+					psmove_set_leds(controller, (char) floor(game_structs[i].counter), 0, 0);
+					psmove_update_leds(controller);
+
+					// char red = (char) floor(game_structs[i].counter);
+					// printf("Controller %d descending: %d\n", i, (int) red);
+				}
+				else if (game_structs[i].state == DONT_MOVE)
+				{
+					psmove_set_leds(controller, 0, 0, 255);
+					psmove_update_leds(controller);
+
+					game_structs[i].dont_move_counter -= 0.02;
+					if (game_structs[i].dont_move_counter < 0)
+					{
+						game_structs[i].state = FINITO;
+					}
 				}
 
-				if (game_structs[i].counter > 0.02)
-				{
-					game_structs[i].counter -= 0.02;
-				}
-				psmove_set_leds(controller, (char) floor(game_structs[i].counter), 0, 0);
-				psmove_update_leds(controller);
-
-				char red = (char) floor(game_structs[i].counter);
-				printf("Controller %d descending: %d\n", i, (int) red);
+				print_debug(game_structs[i], i);
 			}
 		}
-
 	}
 
 	// decide who to blink
 	PSMove *blink_controller = NULL;
 	for (int i = 0; i < numControllers; ++i)
 	{
-		if (game_structs[i].counter > 100)
+		printf("Counter: %.2f\n", game_structs[i].counter);
+		if (game_structs[i].counter >= 20)
 		{
 			blink_controller = game_structs[i].controller;
 			break;
