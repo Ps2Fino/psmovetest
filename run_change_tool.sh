56f93cc12ad6ae58929ba1ed702eb6c6c1e66bc5
Dan Finnegan <dan.finnegan@somethinelse.com>
2014-06-14 12:01:35 +0300

    Fixed the merge errors as to be honest I don't fully understand the git merge operation from command line

--- run_change_tool.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Just run the install tool to fix the library paths
-install_name_tool -change libpsmoveapi.3.dylib /Users/danielfinnegan/Documents/UBISS-Summer-School-2014/psmoveapi_3.0.0_osx108/opt/psmoveapi_3.0.0/lib/libpsmoveapi.3.dylib test
